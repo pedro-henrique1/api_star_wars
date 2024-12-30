f35f5efefac8390eb696dda038bfd395f9097b51
Pedro Henrique Silva Rodrigues <ph8464803@gmail.com>
2024-12-29 22:07:37 -0300

    init commit

---
+++ src/main/java/backend/api_star_wars/ApiStarWarsApplication.java
@@ -0,0 +1,13 @@
+package backend.api_star_wars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiStarWarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiStarWarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend/api_star_wars/ApiStarWarsApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.api_star_wars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiStarWarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
